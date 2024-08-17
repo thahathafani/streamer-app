27144752984420af78be46dd4ab5bbd9ab899a71
thahathafani <thahafazzin@gmail.com>
2024-08-17 16:25:27 +0530

    button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className="text-red-500 font-bold">hello here</p>
+    <Button>hello</Button>
   );
 }
