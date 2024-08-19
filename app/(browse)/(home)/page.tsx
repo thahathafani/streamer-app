fab402401053c1347eb775fbbad841eb8309d345
thahathafani <thahafazzin@gmail.com>
2024-08-19 14:22:09 +0530

    minute pauses

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
-      <UserButton/>
+      <h1>Home</h1>
     </div>
   );
 }
--- app/(browse)/_component/navbar/index.tsx
+++ app/(browse)/_component/navbar/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Logo } from './logo'
 import { Search } from './search'
+import Actions from './actions'
 
 function Navbar() {
   return (
     <nav className='fixed top-0 w-full h-20 z-[49] bg-[#272932] px-2 lg:px-4 flex justify-between items-center shadow-sm'>
-        <Logo/>
+        {/* <Logo/> */}
         <Search/>
+        <Actions />
     </nav>
   )
 }
