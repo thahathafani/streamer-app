47173f500fe66fb395c37a43ef9c41bd34426071
thahathafani <thahafazzin@gmail.com>
2024-08-19 11:58:20 +0530

    dir changed

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-export default function Page() {
-  return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
-      <UserButton/>
-    </div>
-  );
-}
