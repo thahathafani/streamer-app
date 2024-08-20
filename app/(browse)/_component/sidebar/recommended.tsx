9e15c75fcd3d13960aafe62cc37ed62a6eeee9c9
thahathafani <thahafazzin@gmail.com>
2024-08-20 15:22:25 +0530

    recommened updated

--- app/(browse)/_component/sidebar/recommended.tsx
+++ app/(browse)/_component/sidebar/recommended.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { User } from '@prisma/client'
-import React from 'react'
+import { User } from '@prisma/client';
+import { useSidebar } from '@/store/use-sidebar';
+import { UserItem } from './user-item';
 
 interface RecommendedProps {
     data: User[];
@@ -10,8 +11,29 @@ interface RecommendedProps {
 const Recommended = ({
     data,
 }: RecommendedProps) => {
+    const { collapsed } = useSidebar((state) => state);
+;
+
+    const showLable = !collapsed && data.length > 0; //-- its should be added only after the prisma and the ngrok setup is completed!
   return (
-    <div>Recommended</div>
+    <div>{showLable && (
+        <div className="pl-6 mb-4">
+            <p className=" text-sm text-muted-foreground">
+                Recommended
+            </p>
+        </div>
+    )}
+    <ul className="space-y-2 px-2">
+        {data.map((user) => (
+            <UserItem
+                key={user.id}
+                username={user.username}
+                imageUrl={user.imageUrl}
+                isLive={true}
+            />
+        ))}
+    </ul>
+    </div>
   )
 }
 
