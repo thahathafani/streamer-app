1db623bc82b8481244a8c47aee1bdb6d7cdb2939
thahathafani <thahafazzin@gmail.com>
2024-08-20 15:24:28 +0530

    user item added but need to updatation

---
+++ app/(browse)/_component/sidebar/user-item.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { usePathname } from "next/navigation";
+import Link from 'next/link';
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import UserAvatar from "@/components/ui/user-avatar";
+
+interface UserItemProps {
+    username: string;
+    imageUrl: string;
+    isLive?: boolean;
+};
+
+export const UserItem = ({
+    username,
+    imageUrl,
+    isLive,
+}: UserItemProps) => {
+
+    const pathname = usePathname();
+    const { collapsed } = useSidebar((state) => state);
+
+    const href = `/${username}`;
+    const isActive = pathname === href;
+  
+    return (
+        <Button
+            asChild
+            variant="ghost"
+            className={cn(
+                "w-full h-12",
+                collapsed ? "justify-center" : "justfy-start",
+                isActive && "bg-accent",
+            )}>
+            <Link href={href}>
+                <div className={cn(
+                    "flex items-center w-full gap-x-4",
+                    collapsed && "justify-center",
+                    )}>
+                    
+                    <UserAvatar
+                        imageUrl={imageUrl}
+                        username={username}
+                        isLive={isLive}
+                    />
+                </div>
+            </Link>
+        </Button>
+    );
+};
