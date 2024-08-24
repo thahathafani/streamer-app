fec5ca8bcefb04fdd84b7870f87be9a8db8c318d
thahathafani <thahafazzin@gmail.com>
2024-08-24 13:03:24 +0530

    recommended services completed

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -1,1 +1,13 @@
 //recommended services for reusing the recommended list on various areas of the projects!
+
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
+
+export const getRecommended = async () => {
+    const users = await db.user.findMany({
+        orderBy: {
+            createdAt: "desc"
+        },
+    });
+    return users;
+}  
