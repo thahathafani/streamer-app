34d0eb363444d9942d8beda556c9017a18524f27
thahathafani <thahafazzin@gmail.com>
2024-08-24 15:06:05 +0530

    user item skeleton added to user-item

--- app/(browse)/_component/sidebar/user-item.tsx
+++ app/(browse)/_component/sidebar/user-item.tsx
@@ -63,3 +63,13 @@ export const UserItem = ({
 };
 
 
+export const UserItemSkeleton = () => {
+    return (
+        <li className="flex items-center gap-x-4 px-3 py-2">
+            <Skeleton className='min-h-[32px] min-w-[32px] rounded-full'/>
+            <div className="flex-1">
+                <Skeleton className='h-6'/>
+            </div>
+        </li>
+    )
+}
