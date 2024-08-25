e3a6fbcbff1eb725a47fd43dd349e0de022f21ec
thahathafani <thahafazzin@gmail.com>
2024-08-25 18:03:42 +0530

    facing issue for recommended skeleton

--- app/(browse)/_component/sidebar/recommended.tsx
+++ app/(browse)/_component/sidebar/recommended.tsx
@@ -39,11 +39,19 @@ const Recommended = ({
 
 export const RecommendedSkeleton = () => {
     return (
+        
         <ul className="px-2">
             {[...Array(3).map((_, i) => (
                 <UserItemSkeleton key={i}/>
             ))]}
+            
         </ul>
+    //     <li className="flex items-center gap-x-4 px-3 py-2">
+    //     <Skeleton className='min-h-[32px] min-w-[32px] rounded-full'/>
+    //     <div className="flex-1">
+    //         <Skeleton className='h-6'/>
+    //     </div>
+    // </li>
     );
 };
 
