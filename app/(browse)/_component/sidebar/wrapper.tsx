49c2fbfe38ea87bce13c3afdaa74adc49b7a58de
thahathafani <thahafazzin@gmail.com>
2024-08-25 18:01:58 +0530

    sidebar refresh removed and skeleton added

--- app/(browse)/_component/sidebar/wrapper.tsx
+++ app/(browse)/_component/sidebar/wrapper.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
     children: React.ReactNode;
@@ -9,7 +12,20 @@ interface WrapperProps {
 export const Wrapper = ({
     children,   
 }: WrapperProps) => {
+    const [isClient, setIsClient] = useState(false);
     const { collapsed } = useSidebar((state) => state);
+
+    useEffect(() => {
+        setIsClient(true)
+    }, []);
+
+    if (!isClient) return (
+        <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r-2 border-[#2d2e35] z-50">
+            <ToggleSkeleton/>
+            <RecommendedSkeleton/>
+        </aside>
+    )
+    
     return(
         <aside className={cn("fixed left-0 flex flex-col w-60 h-full bg-background border-r-2 border-[#2d2e35] z-50",
             collapsed && "w-[70px]"
