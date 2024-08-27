7565d95c4c71ecbf1d6bb45c546b85d23884bd67
thahathafani <thahafazzin@gmail.com>
2024-08-27 13:18:04 +0530

    unknown list removed

--- app/(browse)/_component/sidebar/recommended.tsx
+++ app/(browse)/_component/sidebar/recommended.tsx
@@ -46,12 +46,6 @@ export const RecommendedSkeleton = () => {
             ))]}
             
         </ul>
-    //     <li className="flex items-center gap-x-4 px-3 py-2">
-    //     <Skeleton className='min-h-[32px] min-w-[32px] rounded-full'/>
-    //     <div className="flex-1">
-    //         <Skeleton className='h-6'/>
-    //     </div>
-    // </li>
     );
 };
 
