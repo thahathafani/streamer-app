fad1d464d4dd74db1e6839b4cca36c4fbf2256d2
thahathafani <thahafazzin@gmail.com>
2024-08-28 20:11:44 +0530

    added more intraction follow into user page

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,17 +1,34 @@
+import { notFound } from 'next/navigation';
+import { getUserByUsername } from '@/lib/user-service';
+import React from 'react'
+import { isFollowingUser } from '@/lib/follow-service';
+
 interface UserPageProps {
     params: {
         username: string;
     };
 };
 
-import React from 'react'
-
-const UserPage = ({
+const UserPage = async ({
     params
 }: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    const user = await getUserByUsername(params.username);
+  }
+  if (!user) {
+    notFound();
+  }
+
+  const isFollowing = await isFollowingUser(user.id);
+
   return (
-    <div>
-        User: {params.username}
+    <div className="flex flex-col gap-y-4">
+      <p>username: {user.username}</p>
+      <p>user email: {user.email}</p>
+      <p>is Following: {`${isFollowing}`}</p>
+
     </div>
   )
 }
