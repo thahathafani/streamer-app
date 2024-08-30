9316c53036facee54f59e8e651192ff402daef1b
thahathafani <thahafazzin@gmail.com>
2024-08-30 19:57:32 +0530

    follow btn added to userpage

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from 'next/navigation';
 import { getUserByUsername } from '@/lib/user-service';
 import React from 'react'
 import { isFollowingUser } from '@/lib/follow-service';
+import { Actions } from './_components/actions';
 
 interface UserPageProps {
     params: {
@@ -28,7 +29,7 @@ const UserPage = async ({
       <p>username: {user.username}</p>
       <p>user email: {user.email}</p>
       <p>is Following: {`${isFollowing}`}</p>
-
+      <Actions/>
     </div>
   )
 }
