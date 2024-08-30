a40adff58060790e4e2d75f7b9cdde1e35e7e975
thahathafani <thahafazzin@gmail.com>
2024-08-30 23:16:44 +0530

    instant transition added: follow api action in client side

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { startTransition, useTransition } from "react";
+import { onFollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 
 export const Actions = () => {
+
+    const [isPending, stratTransition] = useTransition();
+
+    const onClick = () => {
+        startTransition(() => {
+            onFollow("hellooo");
+        })
+}
     return(
-        <Button className="bg-blue-500 text-white hover:bg-gray-500">
+        <Button disabled={isPending} onClick={onClick} className="bg-blue-500 text-white hover:bg-blue-500">
             Follow
         </Button>
     )
