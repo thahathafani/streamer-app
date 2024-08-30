d79e379a44bb4ab7a5db2416966a56fbbe800f09
thahathafani <thahafazzin@gmail.com>
2024-08-30 19:56:56 +0530

    basic follow button created

---
+++ app/(browse)/[username]/_components/actions.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export const Actions = () => {
+    return(
+        <Button className="bg-blue-500 text-white hover:bg-gray-500">
+            Follow
+        </Button>
+    )
+}
