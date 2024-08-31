7b09368ed1647fdfd07f96a500093ba3b5f680cf
thahathafani <thahafazzin@gmail.com>
2024-08-31 20:47:48 +0530

    follow method added

--- actions/follow.ts
+++ actions/follow.ts
@@ -1,10 +1,39 @@
+// "use server";
+
+// export const onFollow = (id: string) => {
+//     try{
+//         console.log("I am an Api call", id);
+//     } catch (error){
+
+//         throw new Error("Internal Error");
+//     }
+// }
+
+
+// follow.ts
 "use server";
 
-export const onFollow = (id: string) => {
-    try{
-        console.log("I am an Api call", id);
-    } catch (error){
+import { revalidatePath } from "next/cache";
+import { followUser } from "@/lib/follow-service";
 
-        throw new Error("Internal Error");
-    }
-}
+export const onFollow = async (id: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+        try {
+            const followedUser = await followUser(id);
+
+            revalidatePath("/");
+
+            if (followUser) {
+                revalidatePath(`/${followUser.following.username}`);
+            }
+
+            // Simulate an API call with a delay
+            setTimeout(() => {
+                resolve(); // Resolve the promise if the operation succeeds
+            });
+            return followUser;
+        } catch (error) {
+            reject(new Error("Internal Error")); // Reject the promise if there's an error
+        }
+    });
+};
