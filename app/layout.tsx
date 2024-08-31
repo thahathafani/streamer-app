661f6a0f9cf3140e7db2016fbeb0619e79d1e840
thahathafani <thahafazzin@gmail.com>
2024-09-01 00:12:53 +0530

    adjust

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import { dark } from '@clerk/themes';
 import { ThemeProvider } from 'next-themes';
 import type { Metadata } from "next";
